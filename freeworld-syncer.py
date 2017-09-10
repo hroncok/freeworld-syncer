ba14b1b46319c811a67f84dd54d2bc616f6c0916
Miro Hrončok <miro@hroncok.cz>
2017-09-10 11:05:56 +0200

    Add tests

--- freeworld-syncer.py
+++ freeworld-syncer.py
@@ -1,3 +1,4 @@
+import pytest
 import re
 import requests
 
@@ -16,6 +17,16 @@ RE_STATUS = re.compile(r'<img class="stateimg" src="/koji-static/images/\w+.'
                        r'png" title="(\w+)" alt="\w+"/>', re.ASCII)
 
 
+@pytest.fixture(params=('fedora', 'rpmfusion', 'centos'))
+def koji_pkgname(request):
+    d = {
+        'fedora': (FEDORA_KOJI, 'fedora-release'),
+        'rpmfusion': (FUSION_KOJI, 'rpmfusion-free-release'),
+        'centos': ('https://cbs.centos.org/koji/', 'centos-release-docker')
+    }
+    return d[request.param]
+
+
 def koji_search_package(koji, pkgname):
     url = koji + 'search'
     r = requests.post(url, data={
@@ -27,6 +38,13 @@ def koji_search_package(koji, pkgname):
     return r.text  # HTML
 
 
+def test_koji_search_package(koji_pkgname):
+    koji, pkgname = koji_pkgname
+    html = koji_search_package(koji, pkgname)
+    assert 'Information for package' in html
+    assert pkgname in html
+
+
 def koji_builds(koji, pkgname):
     html = koji_search_package(koji, pkgname)
     builds = RE_BUILDS.findall(html)
@@ -35,8 +53,19 @@ def koji_builds(koji, pkgname):
         yield build[1], int(build[0]), statuses[idx]
 
 
-for build in koji_builds(FEDORA_KOJI, FEDORA_PKGNAME):
-    print(build)
+def test_koji_builds(koji_pkgname):
+    koji, pkgname = koji_pkgname
+    builds = list(koji_builds(koji, pkgname))
+    for build in builds:
+        assert build[0].startswith(pkgname)
+        assert isinstance(build[1], int)
+        assert build[2] in ('complete', 'deleted', 'canceled', 'free',
+                            'open', 'failed', 'closed')
 
-for build in koji_builds(FUSION_KOJI, FUSION_PKGNAME):
-    print(build)
+
+if __name__ == '__main__':
+    for build in koji_builds(FEDORA_KOJI, FEDORA_PKGNAME):
+        print(build)
+
+    for build in koji_builds(FUSION_KOJI, FUSION_PKGNAME):
+        print(build)
